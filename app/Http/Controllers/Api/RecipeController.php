3b97da98552df0e4e2bdf3a5b2cdacd413267b6a
Kara Lane Zurbano <klanezurbano@gmail.com>
2023-08-16 05:37:00 +0800

    :sparkles: Implement GET /recipes and GET /recipes/:id

---
+++ app/Http/Controllers/Api/RecipeController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RecipeResource;
+use App\Models\Recipe;
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $query = Recipe::query();
+
+        if (isset($request->category)) {
+            $query->where('category', $request->category);
+        }
+
+        return RecipeResource::collection($query->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Recipe $recipe)
+    {
+        return RecipeResource::make($recipe);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
