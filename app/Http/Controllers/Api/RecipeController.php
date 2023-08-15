bcc09d6a5662b9e78d11926355f81bb3adbfaa35
Kara Lane Zurbano <klanezurbano@gmail.com>
2023-08-16 05:58:22 +0800

    :sparkles: Implement PATCH and DELETE /recipes/:id

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RecipeStoreRequest;
+use App\Http\Requests\RecipeUpdateRequest;
 use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
@@ -50,16 +51,39 @@ class RecipeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(RecipeUpdateRequest $request, Recipe $recipe)
     {
-        //
+        if (isset($request->name)) {
+            $recipe->name = $request->name;
+        }
+
+        if (isset($request->instructions)) {
+            $recipe->instructions = $request->instructions;
+        }
+
+        if (isset($request->category)) {
+            $recipe->category = $request->category;
+        }
+
+        if (isset($request->userId)) {
+            $recipe->user_id = $request->userId;
+        }
+
+        $recipe->save();
+
+        return RecipeResource::make($recipe);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Recipe $recipe)
     {
-        //
+        $recipe->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully deleted'
+        ]);
     }
 }
