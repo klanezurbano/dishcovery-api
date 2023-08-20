d8f85854ab14e1eb41c11eb22b32ecdc343989b5
Kara Lane Zurbano <klanezurbano@gmail.com>
2023-08-20 22:31:56 +0800

    :sparkles: Implement DELETE /user

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Recipe;
+use App\Models\RecipeIngredient;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        try {
+            $recipes = Recipe::where('user_id', $user->id)->get();
+
+            foreach ($recipes as $recipe) {
+                RecipeIngredient::where('recipe_id', $recipe->id)->delete();
+                $recipe->delete();
+            }
+
+            $user->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Successfully deleted'
+            ]);
+        } catch (Exception $err) {
+            return response()->json([
+                'success' => false,
+                'message' => $err
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('users', UserController::class);
     Route::apiResource('recipes', RecipeController::class);
 });
